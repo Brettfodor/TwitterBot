5d96297d6c1be0f21e3f51d835d8b19f52c6bc46
Brett Fodor <54860489+Brettfodor@users.noreply.github.com>
2021-03-25 16:32:17 -0400

    rewteet

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '5o4VjfaPJoqL7wmbZNNQIDn72',  
+    consumer_secret: 'O33sHXO2jGVgqVcq0JTCnpdLwHpnOmDASNbf32XIhm5ThexIpA',
+    access_token: '127961258-diBzYfdLvzfwWiaOgUikS7KIKn84lzUwsm8jDLbp',  
+    access_token_secret: 'LVUOHCf4v8aBuudnVZ6RCPdNyArwbmXHuaMeFfw6uBlJt'
+  }
